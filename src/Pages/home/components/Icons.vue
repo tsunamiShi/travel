2f9a679f0499527570516056ee72ca8d8ba2f187
tsunamiShi <tsunamiShi@gmail.com>
2018-09-14 11:09:44 +0800

    add recommend

--- src/Pages/home/components/Icons.vue
+++ src/Pages/home/components/Icons.vue
@@ -82,32 +82,34 @@ export default {
     .icons >>> .swiper-container
         height 0
         padding-bottom 50%
-    .icon 
-        position relative
-        overflow hidden
-        float left
-        width 25%
-        padding-bottom 25%
-        height 0
-        .icon-img
-            position absolute
-            top 0
-            left 0
-            right 0
-            bottom .44rem
-            text-align center
-            box-sizing border-box
-            padding-top .1rem
-            .icon-img-content
-                height 100%
-        .icon-desc
-            position absolute
-            left 0
-            right 0
-            bottom 0
-            height .44rem
-            line-height .44rem
-            text-align center
-            color $darkTextColor
-            ellipsis()
+    .icons
+        margin-top .1rem
+        .icon 
+            position relative
+            overflow hidden
+            float left
+            width 25%
+            padding-bottom 25%
+            height 0
+            .icon-img
+                position absolute
+                top 0
+                left 0
+                right 0
+                bottom .44rem
+                text-align center
+                box-sizing border-box
+                padding-top .1rem
+                .icon-img-content
+                    height 100%
+            .icon-desc
+                position absolute
+                left 0
+                right 0
+                bottom 0
+                height .44rem
+                line-height .44rem
+                text-align center
+                color $darkTextColor
+                ellipsis()
 </style>
